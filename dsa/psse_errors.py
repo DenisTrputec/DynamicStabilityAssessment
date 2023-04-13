e4bb0b338f6425cf03f2bc325fa5f73852e4de1d
DenisTrputec <denistrputec@gmail.com>
2023-04-13 09:10:19 +0200

    Deleted file psse_errors

--- dsa/psse_errors.py
+++
@@ -1,9 +0,0 @@
-read = {
-    0: "no error occurred",
-    1: "invalid NUMNAM value",
-    2: "invalid revision number",
-    3: "unable to convert file",
-    4: "error opening temporary file",
-    10: "error opening IFILE",
-    11: "prerequisite requirements for API are not met"
-}
